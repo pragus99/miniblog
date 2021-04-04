f343124cbe74d4c5dda49b591f150f16a8b113b1
prayogo bagus suntoro <prayogo0399@gmail.com>
2021-04-04 20:53:34 +0700

    create component for list of blogs

---
+++ src/components/BlogList.js
@@ -0,0 +1,19 @@
+// const BlogList = (props) => {
+//   const blogs = props.blogs;
+//   const title = props.title;
+// OR
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
